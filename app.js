f3f380f5423e2406d65f1e26c7b7f176bb719b41
Eduart Zina <Eduart.Zina@coxautoinc.com>
2023-11-06 14:57:27 -0600

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const one = 34
+const two = 35
+const three = 36
+
+function test(numb1, numb2, numb3) {
+    let sum = numb1 + numb2 + numb3
+    return sum;
+}
+test(one, two, three)
+exports.test = test
---
+++ test/app.test.js
@@ -0,0 +1,22 @@
+const {expect} = require('chai')
+const appD = require('../app')
+
+
+describe("when open test", function() {
+    it("should start one", function() {
+        let a = 4
+        let b = 4
+        expect(a).to.equal(b)
+    })
+    it("should start two", function() {
+        let a = 4
+        let b = 3
+        expect(a).not.to.equal(b)
+    })
+    it("should start three", function() {
+        let a = appD.test(12,2,3)
+        let b = 17
+        expect(a).to.equal(b)
+    })
+
+})
