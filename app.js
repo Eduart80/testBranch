e4eb666c2d14d665e1ad9996192b02c8dbe2bd82
Edd <71289882+Eduart80@users.noreply.github.com>
2023-11-06 15:48:13 -0600

    three (#7)
    Co-authored-by: Eduart Zina <Eduart.Zina@coxautoinc.com>

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ function test(numb1, numb2, numb3) {
     return sum;
 }
 test(one, two, three)
-const something = "text string with one and two"
+const something = "text string with one and two and three elements"
+const something22 = "text string with one and two and three elements"
 exports.test = test
